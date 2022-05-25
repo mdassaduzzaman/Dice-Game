1396e7eebec0981e6867f0faf40a0aa3b10982c7
Assaduzzaman <md.assaduzzaman66@gmail.com>
2022-05-25 16:31:49 +0600

    dice game with javascript DOM

---
+++ index.js
@@ -0,0 +1,22 @@
+// Dice One
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+document
+  .querySelector(".img1")
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+// Dice Two
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document
+  .querySelector(".img2")
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+//  Show who wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Win :)";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Playwr 2 Win :)";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!!";
+}
